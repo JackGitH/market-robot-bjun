ac5ef80a3f24cb511a4a8f42682737d78bd5a6ad
jack <gainichengzhenmafanya@gmail.com>
2019-08-29 18:04:15 +0800

    main方法提前到上级目录

---
+++ src/main/java/com/example/MarketRobotApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketRobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketRobotApplication.class, args);
+	}
+
+}
